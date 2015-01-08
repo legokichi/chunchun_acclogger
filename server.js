13b163e8e76792725105b658c1d8c9b170dcc1e4
Legokichi Duckscallion <legokichi@duxca.com>
2015-01-08 16:05:39 +0900

    new way

--- server.js
+++ server.js
@@ -4,28 +4,18 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var fs = require('fs');
 
-var filename = 'devicemotion.csv';
-var filename2 = 'deviceorientation.csv';
-var filename3 = 'state.csv';
-
-fs.writeFile(filename, ["timeStamp","x","y","z","gx","gy","gz"].join(",")+"\n", function (err) {});
-fs.writeFile(filename2,["timeStamp","alpha", "beta", "gamma"].join(",")+"\n", function (err) {});
-
 app.use(express.static(__dirname));
 
 server.listen(8080);
 
 io.on('connection', function (socket) {
   var state = "start";
+  var filename = 'devicemotion'+Date.now()+'.csv';
+  fs.writeFile(filename, ["timeStamp","alpha", "beta", "gamma","x","y","z","gx","gy","gz","label"].join(",")+"\n", function (err) {});
   socket.on('devicemotion', function (arr) {
     var csv = arr.concat(state).join(",")+"\n";
     console.log("mot", csv);
     fs.appendFile(filename, csv, function (err) {});
-  });
-  socket.on('deviceorientation', function (arr) {
-    var csv = arr.concat(state).join(",")+"\n"
-    console.log("ori", csv);
-    fs.appendFile(filename2, csv, function (err) {});
   });
   socket.on('state', function (arr) {
     state = arr[1];
--- viewer.py
+++ viewer.py
@@ -8,63 +8,57 @@ def getRows(reader):
         rows.append(row)
     return rows
 
-dir = "./sitting/"
+dir = ""
 
-rows2 = getRows(csv.reader(open(dir+"deviceorientation.csv", 'rb')))[20:]
-timeStamps2 = []
+timeStamps = []
 alpha = []
 beta = []
 gamma = []
-for row in rows2:
-    timeStamps2.append(int(row[0]))
-    alpha.append(float(row[1]))
-    beta.append(float(row[2]))
-    gamma.append(float(row[3]))
-norms2 = []
-for i, _ in enumerate(alpha):
-    norms2.append(abs(alpha[i]) + abs(beta[i]) + abs(gamma[i]))
-times2 = map(lambda n:n-timeStamps2[0], timeStamps2)
-
-rows = getRows(csv.reader(open(dir+"devicemotion.csv", 'rb')))[20:]
-timeStamps = []
 x = []
 y = []
 z = []
-for row in rows:
+gx = []
+gy = []
+gz = []
+
+for row in getRows(csv.reader(open(dir+"devicemotion.csv", 'rb')))[40:]:
     timeStamps.append(int(row[0]))
-    x.append(float(row[1]))
-    y.append(float(row[2]))
-    z.append(float(row[3]))
+    alpha.append(float(row[1]))
+    beta.append(float(row[2]))
+    gamma.append(float(row[3]))
+    x.append(float(row[4]))
+    y.append(float(row[5]))
+    z.append(float(row[6]))
+    gx.append(float(row[7]))
+    gy.append(float(row[8]))
+    gz.append(float(row[9]))
 norms = []
 for i, _ in enumerate(x):
     norms.append(abs(x[i]) + abs(y[i]) + abs(z[i]))
 times = map(lambda n:n-timeStamps[0], timeStamps)
 
-
-
 widnowSize = 16
 sampleRate = int(1000/(times[1]-times[0]))
 
 subplot(6,1,1)
-plot(times2, alpha)
-plot(times2, beta)
-plot(times2, gamma)
+plot(times, alpha)
+plot(times, beta)
+plot(times, gamma)
 
 subplot(6,1,2)
-plot(times2, norms2)
+plot(times, gx)
+plot(times, gy)
+plot(times, gz)
 
 subplot(6,1,3)
-specgram(norms2, NFFT=widnowSize, Fs=sampleRate, noverlap=widnowSize-1)
-
-subplot(6,1,4)
 plot(times, x)
 plot(times, y)
 plot(times, z)
 
-subplot(6,1,5)
+subplot(6,1,4)
 plot(times, norms)
 
-subplot(6,1,6)
+subplot(6,1,5)
 specgram(norms, NFFT=widnowSize, Fs=sampleRate, noverlap=widnowSize-1)
 
 show()
